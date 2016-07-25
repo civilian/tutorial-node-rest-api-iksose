8e12cc904f199c7c3d9ab316ac940fae6da81e46
Vagrant User <vagrant@vagrant.vm>
2016-07-25 23:32:59 +0000

    Tutorial finished.

--- nodecellar/routes/wines.js
+++ nodecellar/routes/wines.js
@@ -1,7 +1,116 @@
-exports.findAll = function(req, res) {
-    res.send([{name:'wine1'}, {name:'wine2'}, {name:'wine3'}]);
-};
+var mongo = require('mongodb');
+
+var Server = mongo.Server;
+var Db = mongo.Db;
+var ObjectID = mongo.ObjectID;
+
+var server = new Server('localhost', 27017, {auto_reconnect: true});
+var db = new Db('winedb', server);
+
+db.open(function(err, db) {
+    if(!err) {
+        console.log("Connected to 'winedb' database");
+        db.collection('wines', {strict:true}, function(err, collection) {
+            if(err){
+                console.log("The 'wines' collection doesn't exist. Creating it with sample data...");
+                populateDB();
+            }
+        });
+    }
+});
 
 exports.findById = function(req,res) {
-    res.send({id:req.params.id, name: "The name", description: "description"});
+    var id = req.params.id;
+    console.log('Retrieving wine: '+ id);
+    db.collection('wines', function(err, collection) {
+        collection.findOne({'_id':new ObjectID(id)}, function (err, item) {
+            res.send(item);
+        });
+    });
+};
+
+exports.findAll = function(req, res) {
+    db.collection('wines', function(err, collection) {
+        collection.find().toArray(function(err, items) {
+            res.send(items);
+        });
+    });
+};
+
+exports.addWine = function(req, res) {
+    var wine = req.body;
+    console.log('Adding wine: '+ JSON.stringify(wine));
+    db.collection('wines', function(err, collection) {
+        collection.insert(wine, {safe:true}, function(err, result) {
+            if(err){
+                res.send({'error': 'An error ocurred'});
+            } else {
+                console.log('Success: ' + JSON.stringify(result[0]));
+                res.send(result[0]);
+            }
+        });
+    });
+};
+
+exports.updateWine = function (req, res){
+    var id = req.params.id;
+    var wine = req.body;
+    console.log('Updating wine: ' + id);
+    console.log(JSON.stringify(wine));
+    db.collection('wines', function(err, collection) {
+        collection.update({'_id':new ObjectID(id)}, wine, {safe:true}, function(err, result) {
+            if(err){
+                console.log('Error updating wine: '+ err);
+                res.end({'error': 'An error has ocurred'});
+            } else {
+                console.log('' + result + 'document(s) updated');
+                res.send(wine);
+            }
+        });
+    });
+};
+
+exports.deleteWine = function (req, res) {
+    var id = req.params.id;
+    console.log('Deleting wine: ' +id);
+    db.collection('wines', function (err, collection) {
+        collection.remove({'_id':new ObjectID(id)}, {safe:true}, function(err, result) {
+            if (err){
+                res.send({'error': 'An error has ocurred - ' + err});
+            } else {
+                console.log('' + result + 'document(s) deleted');
+                res.send(req.body);
+            }
+        });
+    });
+};
+
+
+/*--------------------------------------------------------*/
+// Populate database with sample data -- Only used once: the first time the application is started.
+// You'd typically not find this code in real-life app, since the database already exists.
+var populateDB = function() {
+    var wines = [
+    {
+        name: "CHATEAU DE SAINT COSME",
+        year: "2009",
+        grapes: "Grenache / Syrah",
+        country: "France",
+        region: "Southern Rhone",
+        description: "The armos of fruit and spice...",
+        picture: "saint_cosme.jpg"
+    }, 
+    {
+        name: "LAN RIOJA CRIANZA",
+        year: "2006",
+        grapes: "Tempranillo",
+        country: "Spain",
+        region: "Rioja",
+        description: "A resourgence of interest in boutique vineyards...",
+        picture: "lan_rioja.jpg"
+    }];
+
+    db.collection('wines', function (err, collection){
+        collection.insert(wines,{safe:true}, function (err, result) {});
+    });
 };
--- nodecellar/server.js
+++ nodecellar/server.js
@@ -3,8 +3,17 @@ var wines = require('./routes/wines.js');
 
 var app = express();
 
+app.configure(function() {
+    app.use(express.logger('dev')); /*'default', 'short', 'tiny', 'dev' */
+    app.use(express.bodyParser());
+});
+
 app.get('/wines', wines.findAll);
 app.get('/wines/:id', wines.findById);
+app.post('/wines', wines.addWine);
+app.put('/wines/:id', wines.updateWine);
+app.delete('/wines/:id', wines.deleteWine);
+
 
 app.listen(50780);
 console.log("Listening on port 50780...");
